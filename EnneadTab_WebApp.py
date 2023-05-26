4d52f13a6ae079d7e0a4308c86f3642155dcaa6e
Sen Zhang EA Laptop <szhang@ennead.com>
2023-05-26 16:25:48 -0400

    first draft

--- EnneadTab_WebApp.py
+++ EnneadTab_WebApp.py
@@ -1,6 +1,20 @@
+##
+# 
+# python -m streamlit run EnneadTab_WebApp.py
+
 import streamlit as st
+import os
+import getpass
+
+user_name = getpass.getuser()
+print (user_name)
 
 
 st.title('EnneadTab for Web')
 
-st.subheader('Hello User A')
+
+st.subheader('Hello {}'.format(user_name))
+
+
+
+print ("Done!")
