f838350cee323396349e8669af7d292da4feaf34
Sen Zhang EA Laptop <szhang@ennead.com>
2023-05-26 19:41:54 -0400

    try2

--- data_receiver.py
+++ data_receiver.py
@@ -1,16 +1,16 @@
 import streamlit as st
 from flask import Flask, request
-from streamlit.report_thread import get_report_ctx
+#from streamlit.report_thread import get_report_ctx
 
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
 def receive_data():
-    ctx = get_report_ctx()
-    session_id = ctx.session_id
+   # ctx = get_report_ctx()
+    #session_id = ctx.session_id
     data = request.json
     # Process the received data
-    st.write("Session ID:", session_id)
+    #st.write("Session ID:", session_id)
     st.write("Received data:", data)
     return 'Data received successfully!'
 
